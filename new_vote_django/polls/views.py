146b9bfaad9ad87e4dd9c96056b6cf2959b3b471
Shakboo Zhou <910298576@qq.com>
2017-11-03 20:36:00 +0800

    1

--- new_vote_django/polls/views.py
+++ new_vote_django/polls/views.py
@@ -3,7 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello!')
+    latest_question_list = Question.objects.order_by('-pub-date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."% question_id
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
