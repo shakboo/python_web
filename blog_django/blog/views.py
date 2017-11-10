7331ac7d69dc5380c31e720f4f265c2b6a2336fd
Shakboo Zhou <910298576@qq.com>
2017-11-10 19:54:02 +0800

    merge views

--- blog_django/blog/views.py
+++ blog_django/blog/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time') #-表示逆序
+    return render(request,'blog/index.html',context={
+        'post_list' : post_list
+    })
