d020decc356ce27edb30ee69aa2fe34a7ee33cbf
Shakboo Zhou <910298576@qq.com>
2017-11-14 20:58:54 +0800

    1

--- new_vote_django/new_vote_django/settings.py
+++ new_vote_django/new_vote_django/settings.py
@@ -117,6 +117,8 @@ USE_I18N = True
 
 USE_L10N = True
 
+AUTH_USER_MODEL = 'polls.User'
+
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
--- new_vote_django/polls/models.py
+++ new_vote_django/polls/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -20,3 +21,9 @@ class Choice(models.Model):
 
     def __unicode__(self):
         return self.choice_text
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
